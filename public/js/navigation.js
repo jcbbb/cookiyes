bc15fc8d3bf5084ea95357b9bff34a84228c2531
jcbbb <avaz28082000@gmail.com>
2023-10-05 23:55:31 +0500

    test

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -73,22 +73,22 @@ export class Navigation {
     let is_back = this.current_entry_index > (e.state?.index || -1);
     let delta = is_back && this.canGoBack ? -1 : 1;
 
+    this.current_entry_index = this.current_entry_index + delta;
+    this.save_state();
+
     let navigate_handlers = this.listeners.get("navigate");
 
     for (let handle of navigate_handlers) {
       let event = new NavEvent({
-        destination: this.entries[this.current_entry_index + delta],
+        destination: this.entries[this.current_entry_index],
         navigationType: "traverse",
-        from: this.entries[this.current_entry_index]
+        from: this.entries[this.current_entry_index - delta]
       });
       await handle(event);
       if (event.handler) {
         await event.handler();
       }
     }
-
-    this.current_entry_index = this.current_entry_index + delta;
-    this.save_state();
   }
 
 
@@ -123,15 +123,17 @@ export class Navigation {
 
   async navigate(url, options = {}) {
     this.clean();
-    let destination = new NavEntry({ url: new URL(url, window.location.origin).href, index: this.current_entry_index + 1 });
-    // TODO: we are not pushing to history if no events defined; Maybe fix?
+    this.current_entry_index += 1
+    let destination = new NavEntry({ url: new URL(url, window.location.origin).href, index: this.current_entry_index });
+    this.entries.push(destination);
+    this.save_state();
     let navigate_handlers = this.listeners.get("navigate");
     for (let handle of navigate_handlers) {
       let event = new NavEvent({
         destination,
         navigationType: options.history || "push",
         info: options.info,
-        from: this.entries[this.current_entry_index]
+        from: this.entries[this.current_entry_index - 1]
       });
 
       await handle(event);
@@ -142,20 +144,20 @@ export class Navigation {
 
     if (options.history === "replace") window.history.replaceState(destination, "", destination.url);
     else window.history.pushState(destination, "", destination.url);
-    this.current_entry_index += 1
-    this.entries.push(destination);
-    this.save_state();
   }
 
   async back() {
     if (!this.canGoBack) return;
-    let destination = this.entries[this.current_entry_index - 1];
+    this.current_entry_index -= 1;
+    this.save_state();
+
+    let destination = this.entries[this.current_entry_index];
     let navigate_handlers = this.listeners.get("navigate");
     for (let handle of navigate_handlers) {
       let event = new NavEvent({
         destination,
         navigationType: "traverse",
-        from: this.entries[this.current_entry_index]
+        from: this.entries[this.current_entry_index + 1]
       });
 
       await handle(event);
@@ -166,19 +168,19 @@ export class Navigation {
     }
 
     window.history.back();
-    this.current_entry_index -= 1;
-    this.save_state();
   }
 
   async forward() {
     if (!this.canGoForward) return;
-    let destination = this.entries[this.current_entry_index + 1];
+    this.current_entry_index += 1;
+    this.save_state();
+    let destination = this.entries[this.current_entry_index];
     let navigate_handlers = this.listeners.get("navigate");
     for (let handle of navigate_handlers) {
       let event = new NavEvent({
         destination,
         navigationType: "traverse",
-        from: this.entries[this.current_entry_index]
+        from: this.entries[this.current_entry_index - 1]
       });
 
       await handle(event);
@@ -188,8 +190,6 @@ export class Navigation {
     }
 
     window.history.forward();
-    this.current_entry_index += 1;
-    this.save_state();
   }
 
   get currentEntry() {
